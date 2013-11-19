c9b5fdd6eba7c6131b6e387c1df16e55f56b00b5
Dave Hall <dave@etianen.com>
2013-11-19 15:19:15 +0000

    More advanced tests for cross-origin requests

--- cross_origin/tests.py
+++ cross_origin/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.test import TestCase
 from django.test.utils import override_settings
 
-from cross_origin.views import AccessControlMixin
+from cross_origin.views import AccessControlMixin, ACCESS_CONTROL_ALLOW_ORIGIN_ALL
 
 
 class TestCrossOriginView(AccessControlMixin, generic.View):
@@ -13,18 +13,26 @@ class TestCrossOriginView(AccessControlMixin, generic.View):
         return HttpResponse("OK")
 
 
+FOO_DOT_COM = "http://www.foo.com"
+BAR_DOT_COM = "http://www.bar.com"
+
+
 urlpatterns = patterns("",
     url(r'^$', TestCrossOriginView.as_view()),
+    url(r'^restricted/$', TestCrossOriginView.as_view(access_control_allow_origin=(
+        FOO_DOT_COM,
+        BAR_DOT_COM,
+    ))),
 )
 
 
 @override_settings(ROOT_URLCONF="cross_origin.tests")
 class CrossOriginViewTest(TestCase):
 
-    def assertCrossOriginResponse(self, response):
+    def assertCrossOriginResponse(self, response, allow_origin=ACCESS_CONTROL_ALLOW_ORIGIN_ALL):
         self.assertEqual(response["Access-Control-Max-Age"], str(AccessControlMixin.access_control_max_age))
         self.assertEqual(response["Access-Control-Allow-Credentials"], str(AccessControlMixin.access_control_allow_credentials).lower())
-        self.assertEqual(response["Access-Control-Allow-Origin"], "*")
+        self.assertEqual(response["Access-Control-Allow-Origin"], allow_origin)
         self.assertEqual(response["Access-Control-Allow-Headers"], ", ".join(AccessControlMixin.access_control_allow_headers))
 
     def testCrossOriginGetRequest(self):
@@ -40,3 +48,25 @@ class CrossOriginViewTest(TestCase):
         # Test the headers.
         self.assertCrossOriginResponse(response)
         self.assertEqual(response["Access-Control-Allow-Methods"], "GET, HEAD, OPTIONS")
+
+    def testCrossOriginRestictedGet(self):
+        # Test a request to FOO_DOT_COM.
+        response = self.client.get("/restricted/",
+            HTTP_ORIGIN = FOO_DOT_COM,
+        )
+        self.assertEqual(response.content, "OK")
+        self.assertCrossOriginResponse(response, FOO_DOT_COM)
+        # Test a request to BAR_DOT_COM.
+        response = self.client.get("/restricted/",
+            HTTP_ORIGIN = BAR_DOT_COM,
+        )
+        self.assertEqual(response.content, "OK")
+        self.assertCrossOriginResponse(response, BAR_DOT_COM)
+
+    def testCrossOriginRestictedGetFailing(self):
+        # Test a request to somewhere that's not allowed.
+        response = self.client.get("/restricted/",
+            HTTP_ORIGIN = "http://www.example.com/",
+        )
+        self.assertEqual(response.content, "OK")
+        self.assertCrossOriginResponse(response, FOO_DOT_COM)
